fc481539450c2a519519bb04a8e9d6eed520e90f
Mattia Nicolis <mattia.nicolis04@gmail.com>
2026-03-19 12:49:27 +0100

    aggiunto materiale del secondo laboratorio

---
+++ 01-TerzoAnno/02-SecondoSemestre/03-Programmazione&SicurezzaReti/laboratorio/socket/esempio01.c
@@ -0,0 +1,15 @@
+#include "socket/Esempi-client-server/network.h"
+
+int main(void) {
+   socketif_t socket;
+   char request[] = "Ciao sono il client!\n";
+   char response[MTU];
+   char hostAdress[MAXADDRESSLEN];
+   int port;
+   
+   socket = createUDPInterface(200000);
+   UDPSend(socket, request, strlen(request), "127.0.0.1", 35000);
+   UDPReceive(socket, response, MTU, hostAdress, &port);
+   printf("[CLIENT] Ho ricevuto un messaggio da host/porta %s/%d\n", hostAdress, port);
+   printf("[CLIENT] Contenuto %s\n", response);
+}
