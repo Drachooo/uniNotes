a359f13b74c1d21dc66a1566ef85220e6fb4771f
Drago <matteo.drago.vr@gmail.com>
2026-04-16 13:01:56 +0200

    Aggiunto lab 03 basi (da terminare)

--- 01-TerzoAnno/02-SecondoSemestre/01-BasiDiDati/laboratorio/drago/lab03/esercizi.sql
+++ 01-TerzoAnno/02-SecondoSemestre/01-BasiDiDati/laboratorio/drago/lab03/esercizi.sql
@@ -1,161 +1,217 @@
-/*Visualizzare il nome degli insegnamenti offerti dal corso di laurea in 
-Informatica nell’anno accademico 2009/2010.*/
-
-SELECT DISTINCT I. nomeins
-FROM CorsoStudi CS
-  JOIN InsErogato IE ON CS.id = IE. id_corsostudi
-  JOIN Insegn I ON IE. id_insegn = I.id
-WHERE IE. annoaccademico = '2009/2010'
-  AND CS. nome = 'Laurea in Informatica'
 
 
-/*
-Visualizzare, in ordine decrescente rispetto al cognome, tutti i docenti dell’ateneo 
-coinvolti nella docenza di un insegnamento il cui nome contiene la sottostringa 
-“Lingua”, riportando nome e cognome del docente
+/* 
+ESERCIZI LAB 3
 */
 
-SELECT DISTINCT P.Nome, P.Cognome
-FROM persona P
-JOIN Docenza D ON P.id = D.id_persona
-JOIN InsErogato IE ON IE.id = D.id_inserogato
-JOIN Insegn I ON IE. id_insegn = I.id	
-WHERE I.nomeins LIKE '%Lingua%'
-ORDER BY Cognome DESC;
-
-
-/*
-Visualizzare, per ciascun corso di studi della Facoltà di Economia, il numero di 
-docenti che hanno tenuto insegnamenti nel 2006/2007, riportando il nome del 
-corso e il numero richiesto.
+/* 
+Esercizio 1
+Visualizzare il numero di corso studi presenti nella base di dati.
+Soluzione: ci sono 635 corsi di studio.
 */
 
-SELECT CS.nome , COUNT ( DISTINCT D.id_persona ) AS NumDoc
-FROM CorsoStudi CS
-JOIN InsErogato IE ON CS.id = IE.id_corsostudi
-JOIN Docenza D ON IE.id = D.id_inserogato
-JOIN CorsoInFacolta CSF ON CSF.id_corsostudi =CS.id
-JOIN Facolta F ON CSF.id_facolta = F.id
-WHERE IE.annoaccademico = '2006/2007'
-AND F.nome = 'Economia'
-GROUP BY CS.nome;
-
-
-
-/* Esercizio 1 
-Visualizzare il numero di corso studi presenti nella base di dati. 
-Soluzione: ci sono 635 corsi di studio. 
-*/
-SELECT COUNT(id) as numeroCorsi
-FROM corsoStudi
+SELECT COUNT(corsostudi) numero_corso_studi
+FROM corsostudi
 
 /*
-Esercizio 2 
-Visualizzare il nome, il codice, l’indirizzo e l’identificatore del preside di tutte le facoltà. 
-Soluzione: ci sono 8 facoltà. 
+Esercizio 2
+Visualizzare il nome, il codice, l’indirizzo e l’identificatore del preside di tutte le facoltà.
+Soluzione: ci sono 8 facoltà.
 */
 
 SELECT nome, codice, indirizzo, id_preside_persona
 FROM facolta
 
 /*
-Esercizio 3 
-Trovare per ogni corso di studi che ha erogato insegnamenti nel 2010/2011 il suo nome e il nome delle 
-facoltà che lo gestiscono (si noti che un corso può essere gestito da più facoltà). Non usare la relazione 
-diretta tra InsErogato e Facoltà. Porre i risultati in ordine di nome corso studi. 
+Esercizio 3
+Trovare per ogni corso di studi che ha erogato insegnamenti nel 2010/2011 il suo nome e 
+il nome delle facoltà che lo gestiscono (si noti che un corso può essere gestito da più facoltà). 
+Non usare la relazione diretta tra InsErogato e Facoltà. Porre i risultati in ordine di nome corso studi.
 Soluzione: ci sono 211 righe. Le 5 righe dalla X posizione sono:
+nome | nome
+-- -----------------------------------------------+----------------------------------------
+Corso di Perfezionamento IN Traumatologia dentale | Medicina e Chirurgia
+Laurea IN Beni culturali | Lettere e filosofia
+Laurea IN Bioinformatica | Scienze matematiche fisiche e naturali
+Laurea IN Bioinformatica (ordinamento fino ALL’a.a. 2008/09) | Scienze matematiche fisiche e naturali
+Laurea IN Biotecnologie | Scienze matematiche fisiche e naturali
 */
 
-SELECT DISTINCT cs.nome, f.nome
-FROM CorsoStudi cs
-join corsoinfacolta cif on cs.id = cif.id_corsostudi 
-join facolta f on f.id = cif.id_facolta
-join inserogato ins on cs.id = ins.id_corsostudi
-where ins.annoaccademico = '2010/2011'
+SELECT DISTINCT cs.nome, f.nome 
+FROM corsostudi cs
+	JOIN corsoinfacolta CF ON CF.id_corsostudi = cs.id
+	JOIN facolta f ON f.id = CF.id_facolta 
+	JOIN inserogato ins ON ins.id_corsostudi = cs.id
+WHERE ins.annoaccademico = '2010/2011'
+ORDER BY cs.nome
 
 /*
-Esercizio 4  
-Visualizzare il nome, il codice e l’abbreviazione di tutti i corsi di studio gestiti dalla facoltà di Medicina e 
-Chirurgia. 
+Esercizio 4
+Visualizzare il nome, il codice e l’abbreviazione di tutti i corsi di studio gestiti 
+dalla facoltà di Medicina e Chirurgia.
 Soluzione: ci sono 236 righe.
 */
 
 SELECT cs.nome, cs.codice, cs.abbreviazione
-FROM corsoStudi cs
-join corsoinfacolta cif on cs.id = cif.id_corsostudi
-join facolta f on cif.id_facolta = f.id
-where f.nome = 'Medicina e Chirurgia'
-
-
-select *
-from facolta
+FROM corsostudi cs
+	JOIN corsoinfacolta CF ON CF.id_corsostudi = cs.id
+	JOIN facolta f ON f.id = CF.id_facolta
+WHERE f.nome = 'Medicina e Chirurgia'
 
 /*
-Esercizio 5 
-Visualizzare il codice, il nome e l’abbreviazione di tutti corsi di studio che nel nome contengono la 
-sottostringa ’lingue’ (eseguire il confronto usando ILIKE invece di LIKE: in questo modo i caratteri 
-maiuscolo e minuscolo non sono diversi). 
-Soluzione: ci sono 16 righe. 
+Esercizio 5
+Visualizzare il codice, il nome e l’abbreviazione di tutti corsi di studio che nel nome 
+contengono la sottostringa ’lingue’ (eseguire il confronto usando ILIKE invece di LIKE: 
+in questo modo i caratteri maiuscolo e minuscolo non sono diversi).
+Soluzione: ci sono 16 righe.
 */
 
 SELECT cs.codice, cs.nome, cs.abbreviazione
-FROM corsostudi cs
-where nome ilike '%lingue%'
+FROM corsostudi cs 
+WHERE cs.nome ILIKE '%lingue%'
 
 /*
-Esercizio 6 
-Visualizzare le sedi dei corsi di studi in un elenco senza duplicati. 
+Esercizio 6
+Visualizzare le sedi dei corsi di studi in un elenco senza duplicati.
 Soluzione: ci sono 48 righe.
 */
 
-SELECT distinct cs.sede
-FROM corsostudi cs
+SELECT DISTINCT sede
+FROM corsostudi
 
-
-/*
-Esercizio 7 
-Visualizzare solo i moduli degli insegnamenti erogati nel 2010/2011 nei corsi di studi della facoltà di 
-Economia. 
-Si visualizzi il nome dell’insegnamento, il discriminante (attributo descrizione della tabella Discriminante), 
-il nome del modulo e l’attributo modulo. 
-Soluzione: ci sono 27 righe. 
+/* 
+Esercizio 7
+Visualizzare solo i moduli degli insegnamenti erogati nel 2010/2011 nei corsi di studi 
+della facoltà di Economia.
+Si visualizzi il nome dell’insegnamento, il discriminante 
+(attributo descrizione della tabella Discriminante), il nome del modulo e l’attributo modulo.
+Soluzione: ci sono 27 righe.
 */
 
-SELECT DISTINCT i.nomeins, d.descrizione, ins.nomemodulo, ins.modulo, f.nome
-FROM insErogato ins
-join insegn i on ins.id_insegn = i.id
-join discriminante d on d.id = ins.id_discriminante
-join corsoinfacolta cif on cif.id_corsostudi = ins.id_corsostudi
-join facolta f on f.id = cif.id_facolta
-where ins.annoaccademico = '2010/2011' and f.nome = 'Economia' and ins.modulo > 0
+SELECT DISTINCT i.nomeins, d.descrizione, ins.modulo, ins.nomemodulo
+FROM inserogato	ins
+	JOIN insegn i ON i.id = ins.id_insegn	
+	JOIN discriminante d ON d.id = ins.id_discriminante
+	JOIN facolta f ON f.id = ins.id_facolta
+	JOIN corsoinfacolta cif ON cif.id_corsostudi = ins.id_corsostudi
+WHERE ins.annoaccademico = '2010/2011' AND f.nome = 'Economia' AND ins.modulo > 0
+
+/*
+Esercizio 8
+Visualizzare il nome e il discriminante (attributo descrizione della tabella Discriminante) 
+degli insegnamenti erogati nel 2009/2010 che non sono moduli e che hanno 3, 5 o 12 crediti. 
+Si ordini il risultato per discriminante.
+Soluzione: ci sono 724 righe distinte. Le ultime 5 righe sono:
+nomeins | discriminante
+-- ----------------------------------+---------------
+Prova finale | CInt
+Laboratorio di composizione italiana | Cognomi A-K
+Biologia | Cognomi A-L
+Laboratorio di composizione italiana | Cognomi L-Z
+Biologia | Cognomi M-Z
+*/
+
+SELECT DISTINCT i.nomeins, d.descrizione, ins.modulo
+FROM inserogato ins
+	JOIN discriminante d ON d.id = ins.id_discriminante 
+	JOIN insegn i ON i.id = ins.id_insegn 
+WHERE ins.annoaccademico = '2009/2010' AND ins.modulo = 0 AND (ins.crediti = 3 or ins.crediti = 5 or ins.crediti = 12)
+ORDER BY d.descrizione
+
+--altrimenti si poteva utilizzare (per i crediti) ins.crediti IN (3, 5, 12)
+
+/*
+Esercizio 9
+Visualizzare l’identificatore, il nome e il discriminante degli insegnamenti erogati nel 2008/2009 
+che non sono moduli o unità logistiche e con peso maggiore di 9 crediti. Ordinare per nome.
+Soluzione: ci sono 1218 righe. Le 5 righe dalla MXXIII riga sono:
+id | nomeins | discriminante
+-- ---+----------------------------------------+-------------------
+39872 | Storia del diritto medievale e moderno | Matricole pari
+44440 | Storia del diritto medievale e moderno | Matricole dispari
+39724 | Storia del diritto medievale e moderno | Matricole pari
+44428 | Storia del diritto medievale e moderno | Matricole dispari
+44441 | Storia del diritto medievale e moderno | Matricole dispari
+*/
+
+SELECT ins.id, i.nomeins, d.descrizione
+FROM inserogato ins
+	JOIN discriminante d ON d.id = ins.id_discriminante
+	JOIN insegn i ON i.id = ins.id_insegn
+WHERE ins.annoaccademico = '2008/2009' AND ins.modulo = 0 AND ins.crediti > 9
+ORDER BY i.nomeins
+
+/*
+Esercizio 10
+Visualizzare in ordine alfabetico di nome degli insegnamenti 
+(esclusi i moduli e le unità logistiche) erogati nel 2010/2011 nel corso di studi in Informatica, 
+riportando il nome, il discriminante, i crediti e gli anni di erogazione.
+Soluzione: ci sono 26 righe.
+*/
+
+SELECT DISTINCT i.nomeins, d.descrizione, ins.crediti, ins.annoaccademico
+FROM inserogato ins
+	JOIN corsostudi cs ON cs.id = ins.id_corsostudi
+	JOIN insegn i ON i.id = ins.id_insegn
+	JOIN discriminante d ON d.id = ins.id_discriminante
+WHERE ins.annoaccademico = '2010/2011' AND cs.nome LIKE '%Informatica' AND ins.modulo = 0
+ORDER BY i.nomeins ASC 
 
 SELECT *
-from inserogato
-
-
+FROM corsostudi
 
 /*
-
-Esercizio 8 
-Visualizzare il nome e il discriminante (attributo descrizione della tabella Discriminante) degli 
-insegnamenti erogati nel 2009/2010 che non sono moduli o unità logistiche e che hanno 3, 5 o 12 crediti. 
-Si ordini il risultato per discriminante. 
-Soluzione: ci sono 724 righe distinte. Le ultime 5 righe sono:  
+Esercizio 11
+Trovare il massimo numero di crediti associato a un insegnamento fra quelli erogati nel 2010/2011.
+Soluzione: 180
 */
 
-
-SELECT DISTINCT i.nomeins, d.descrizione
-FROM insErogato ins
-join discriminante d on d.id = ins.id_discriminante
-join insegn i on i.id = ins.id_insegn 
-where ins.annoaccademico = '2009/2010' 
-	and ins.crediti IN (3, 5, 12) 
-	and ins.modulo = 0 
-order by d.descrizione
-
+SELECT max(ins.crediti) credito_massimo
+FROM inserogato ins
+WHERE ins.annoaccademico = '2010/2011'
 
 /*
-Esercizio 9 
-Visualizzare l’identificatore, il nome e il discriminante degli insegnamenti erogati nel 2008/2009 che non 
-sono moduli o unità logistiche e con peso maggiore di 9 crediti. Ordinare per nome. 
+Esercizio 12
+Trovare, per ogni anno accademico, il massimo e il minimo numero di crediti erogati tra 
+gli insegnamenti dell’anno.
+Soluzione: ci sono 16 righe.
 */
+
+SELECT ins.annoaccademico, MAX(ins.crediti) massimo_credito, MIN(ins.crediti) minimo_crediti
+FROM inserogato ins
+GROUP BY ins.annoaccademico
+
+/*
+Esercizio 13
+Trovare, per ogni anno accademico e per ogni corso di studi la somma dei crediti erogati 
+(esclusi i moduli e le unità logistiche: vedi nota sopra) e il massimo e minimo numero di crediti degli 
+insegnamenti erogati sempre escludendo i moduli e le unità logistiche.
+Soluzione: ci sono 1587 righe. Le riga relativa alla "Scuola di Specializzazione in Urologia 
+(Vecchio ordinamento)" nell’anno 2011/2012 ha valori 52.00, 10.00 e 162.00.
+*/
+
+SELECT ins.annoaccademico, cs.nome, SUM(ins.crediti), MAX(ins.crediti), MIN(ins.crediti)
+FROM inserogato ins
+	JOIN corsostudi cs ON cs.id = ins.id_corsostudi
+WHERE ins.modulo = 0
+GROUP BY ins.annoaccademico, cs.nome
+ORDER BY cs.nome
+--ordinamento fatto solo per trovare la query della soluzione 
+
+/*
+Esercizio 14
+Trovare per ogni corso di studi della facoltà di Scienze Matematiche Fisiche e Naturali 
+il numero di insegnamenti (esclusi i moduli e le unità logistiche) erogati nel 2009/2010.
+Soluzione: ci sono 19 righe.
+*/
+
+SELECT cs.nome, f.nome, COUNT(ins.id)
+FROM inserogato ins
+	JOIN corsostudi cs ON cs.id = ins.id_corsostudi
+    JOIN CorsoInFacolta cif ON cs.id = cif.id_corsostudi 
+    JOIN facolta f ON f.id = cif.id_facolta
+WHERE ins.modulo = 0 AND ins.annoaccademico = '2009/2010' AND f.nome ILIKE 'Scienze Matematiche Fisiche e Naturali'
+GROUP BY cs.nome, f.nome
+
+SELECT *
+FROM facolta
